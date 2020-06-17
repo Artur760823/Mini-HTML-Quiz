5df3424dc0781c5933bff10919f83aa2cbf92195
Artur <artur.zajaczkowski76@gmail.com>
2020-06-17 12:05:49 +0200

    Modufy js and add Media queries to all

--- main.js
+++ main.js
@@ -2,17 +2,30 @@ const correctAnswers = ['B', 'C', 'B', 'A', 'B', 'C', 'B', 'B', 'B', 'C'];
 const formQuiz = document.querySelector('.quiz');
 const displayScore = document.querySelector('.score');
 
+//Add listening for input - submit
+
 formQuiz.addEventListener('submit', (e) => {
   e.preventDefault();
   let score = 0;
   const userAnswers = [formQuiz.q1.value, formQuiz.q2.value, formQuiz.q3.value, formQuiz.q4.value, formQuiz.q5.value, formQuiz.q6.value, formQuiz.q7.value, formQuiz.q8.value, formQuiz.q9.value, formQuiz.q10.value];
-  //check correct answ
+  //check correct answ & add score for each correct answer
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]) {
       score += 10;
     }
+
   });
-  displayScore.textContent = `You are HTML Ninja in ${score}%`;
+  scrollTo(0, 0);
 
-
+  //score animation
+  let outPutScore = 0;
+  const timer = setInterval(() => {
+    displayScore.textContent = `You are HTML Jedi in ${outPutScore}%`;
+    if (outPutScore === score) {
+      clearInterval(timer)
+    } else {
+      outPutScore++;
+    }
+  }, 10);
 });
+
